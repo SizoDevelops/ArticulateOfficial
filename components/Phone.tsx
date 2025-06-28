81a5ea777d8b6bf36af2deb05cfb2456a2c17208
SizoDevelops <fierceincii@gmail.com>
2025-06-28 16:15:58 +0200

    Updated Phone Wallpaper

--- components/Phone.tsx
+++ components/Phone.tsx
@@ -1,15 +1,27 @@
+"use client"
 import React from 'react'
 import Buttons from './Buttons'
 import styles from './styles/Phone.module.css'
 
 export default function Phone() {
+    // Millitary Time
+    const [time, setTime] = React.useState("");
+    React.useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            const hours = now.getHours().toString().padStart(2, "0");
+            const minutes = now.getMinutes().toString().padStart(2, "0");
+            setTime(`${hours}:${minutes}`);
+        }, 1000);
+        return () => clearInterval(interval);
+    })
   return (
     <div className={styles.body}>
         <span className={styles.outerCover}>
             <span className={styles.outerShine}>
                 <span className={styles.timeAndDate}>
                     <span className={styles.time}>
-                        12:29
+                        {time}
                     </span>
                     <span className={styles.date}></span>
                 </span>
